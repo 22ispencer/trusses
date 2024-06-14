eaac5db6010b253075ad23da76c7a4484483e0e4
Isaac Spencer <22ispencer@gmail.com>
2024-06-14 10:32:48 -0500

    cJSON working

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <cjson/cJSON.h>
 
 int main()
 {
-	printf("Hello, World!");
+	FILE *fp = fopen("example.json", "r");
+	fseek(fp, 0L, SEEK_END);
+	size_t size = ftell(fp);
+	rewind(fp);
+
+	char *buf = malloc(size);
+	fread(buf, sizeof(buf), size, fp);
+	fclose(fp);
+
+	cJSON *json = cJSON_ParseWithLength(buf, size);
+	if (json == NULL) {
+		printf("Unable to parse JSON");
+		return 1;
+	}
+
+	cJSON *message_json = cJSON_GetObjectItem(json, "message");
+	if (message_json != NULL) {
+		printf("message: %s", message_json->valuestring);
+	}
+
+	free(buf);
+
 	return 0;
 }
